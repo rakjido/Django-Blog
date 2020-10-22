309f60a00563138a8e92e3f245325ad376dde0a9
rakjido <ndr.zenn@gmail.com>
2020-10-22 14:10:21 +0900

    [BLD] uWSGI 기초 설정

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hell")
+# import pymysql
+
+# db = pymysql.connect(
+#     host="103.218.156.9", port=33760, user="Lotte", password="akemahdkwpf@Lotte", db="Lotte"
+# )
+
+from db import db
+
+print(db.select("SELECT * from Lotte"))
