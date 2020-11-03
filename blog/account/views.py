ab146979306311c363485e8e8fa06470a8cdfb00
rakjido <ipos222@naver.com>
2020-11-03 18:40:18 +0900

    fix

--- blog/account/views.py
+++ blog/account/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
+from django.core.mail import EmailMessage
 
 from .forms import LoginForm
 from .forms import SignUpForm
@@ -32,12 +33,27 @@ logger = logging.getLogger(__name__)
 
 
 def sign_up(request):
+    """
+    Sign Up
+    """
+    logger.info("sign_up")
     if request.method == "POST":
         signup_form = SignUpForm(request.POST)
         if signup_form.is_valid():
             # signup way 1
             new_user = signup_form.save(commit=False)
             new_user.set_password(signup_form.cleaned_data["password"])
+            # new_user.is_active = False
+            print(request.get_host())
+
+            try:
+                email = EmailMessage("가입 인증", "가입인증메일이에요", to=[new_user.email])
+                email.send()
+            except Exception:
+                logger.error("E-mail error")
+
+            print("email : {}".format(new_user.email))
+            print("is_active: {}".format(new_user.is_active))
             new_user.save()
             return redirect("/")
     else:
